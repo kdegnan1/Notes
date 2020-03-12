703acd0b60f362b03caae0d6d8a58626c4032d18
kdegnan1 <katherine.degnan@gmail.com>
2020-03-12 16:52:24 -0400

    added to notes

--- notes.js
+++ notes.js
@@ -1,13 +1,2 @@
-//send out more apps with outreach 
-//send out 20 more apps befire sunday !
-//finish interview db
-//use new sites to apply to jobs 
-//ttoal 40 apps this week 
-//saturday notes 
-    //10 more apps to send out, need mroe outreach 
-    //make more green
-    //fix interview db challenges 
-    //data structures practice 
-    //event hype app fix updates
-    //
-    //
+
+//March 12 
