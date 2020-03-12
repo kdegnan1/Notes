aa2e28e7db6609eb33c6f979a16fbe4f16940f8d
kdegnan1 <katherine.degnan@gmail.com>
2020-03-12 16:55:29 -0400

    added to notes

--- notes.js
+++ notes.js
@@ -5,3 +5,4 @@
 //going to practice for triple byte quiz 
 // still have to fix updated time on fullstack 
 //practice algorithms 
+// finish last touches on projects so next week can start a new one 
